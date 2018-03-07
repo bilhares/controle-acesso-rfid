d4581b449e31fdff12da470208c8b87997b01eb5
Felipe Bilhares <felipe.bilhares@hotmail.com>
2018-03-07 07:54:43 -0400

    Projeto inicial

---
+++ Controle de acesso/Controle de acesso/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Controle_de_acesso
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.arduino = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbPortas = new System.Windows.Forms.ComboBox();
+            this.txtBaund = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite sua senha:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(7, 42);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(248, 20);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSenha_KeyDown);
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(180, 68);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // arduino
+            // 
+            this.arduino.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.arduino_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConectar);
+            this.groupBox1.Controls.Add(this.txtBaund);
+            this.groupBox1.Controls.Add(this.cbPortas);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 71);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configurações";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtSenha);
+            this.groupBox2.Controls.Add(this.btnEntrar);
+            this.groupBox2.Location = new System.Drawing.Point(13, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(261, 99);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Entrar";
+            // 
+            // cbPortas
+            // 
+            this.cbPortas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPortas.FormattingEnabled = true;
+            this.cbPortas.Location = new System.Drawing.Point(9, 20);
+            this.cbPortas.Name = "cbPortas";
+            this.cbPortas.Size = new System.Drawing.Size(121, 21);
+            this.cbPortas.TabIndex = 0;
+            // 
+            // txtBaund
+            // 
+            this.txtBaund.Location = new System.Drawing.Point(9, 48);
+            this.txtBaund.Name = "txtBaund";
+            this.txtBaund.Size = new System.Drawing.Size(121, 20);
+            this.txtBaund.TabIndex = 1;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(166, 45);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(89, 23);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(286, 221);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.IO.Ports.SerialPort arduino;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtBaund;
+        private System.Windows.Forms.ComboBox cbPortas;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Controle de acesso/Controle de acesso/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO.Ports;
+
+namespace Controle_de_acesso
+{
+    public partial class frmLogin : Form
+    {
+        private ArrayList senhas = new ArrayList();
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            login(txtSenha.Text);
+        }
+
+        private void login(string senha)
+        {
+            if (senha.Trim() == "")
+                MessageBox.Show("Você deve informar uma senha!");
+            else if (senhas.Contains(senha))
+                MessageBox.Show("Login efetuado!");
+            else
+                MessageBox.Show("Senha inválida!");
+
+            txtSenha.Text = "";
+            
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            senhas.Add("admin");
+            senhas.Add("12345");
+            senhas.Add("7497572e");
+
+            string[] p = SerialPort.GetPortNames();
+            cbPortas.DataSource = p;
+
+       }
+
+        private void txtSenha_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(Keys.Enter == e.KeyCode)
+                login(txtSenha.Text);
+        }
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                arduino.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message);
+            }
+        }
+
+        private void arduino_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(loginArduino));
+        }
+
+        private void loginArduino(object o, EventArgs e)
+        {
+            string teste = arduino.ReadLine().Replace("\r", "");
+            login(teste);
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            txtBaund.Enabled = !txtBaund.Enabled;
+            try
+            {                
+                if (btnConectar.Text == "Conectar")
+                {
+                    arduino.PortName = cbPortas.SelectedValue.ToString();
+                    arduino.BaudRate = Int32.Parse(txtBaund.Text);
+                    arduino.Open();
+                    btnConectar.Text = "Desconectar";
+                    MessageBox.Show("Conexão efetuada!");
+                }
+                else
+                {
+                    arduino.Close();
+                    btnConectar.Text = "Conectar";
+                    MessageBox.Show("Desconectado com sucesso!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message);
+            }
+        }
+
+    }
+}
